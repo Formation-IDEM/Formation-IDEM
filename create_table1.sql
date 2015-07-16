137161191d63f91ce1ecb1542bbed113734a042f
dorandeu <jiludo@gmail.com>
2015-07-16 16:41:53 +0200

    Delete create_table1.sql

--- create_table1.sql
+++
@@ -1,5 +0,0 @@
-﻿
- CREATE TABLE lecteur (nom VARCHAR(50), age SMALLINT, idole VARCHAR(50));
- CREATE TABLE auteur (nom_auteur VARCHAR(50), oeuvre VARCHAR(120));
- INSERT INTO lecteur (nom, age, idole) VALUES ('Corinne', 15, 'Stendhal'),('germaine', 17, 'Stendhal'),('Justine', 19, 'Hugo'),('Anne', 21, 'Sartre'); 
- INSERT INTO auteur (nom_auteur, oeuvre) VALUES ('Sartre', 'les mots'),('Sartre', 'l''être et le néant'),('Stendhal', 'Le rouge et le noir'),('Baudelaire', 'les fleurs du mal');
