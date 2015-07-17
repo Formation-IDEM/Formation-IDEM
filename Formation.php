03098ea0302aa96c713f480bd7ffdc468509c50e
Julien Delacourt <jaydelacofficial@gmail.com>
2015-07-17 12:31:18 +0200

    envoi formation.php

---
+++ Formation.php
@@ -0,0 +1,20 @@
+<?php
+   class Formation{
+   	
+		private $_id;
+		private $_effectif_moyen;
+		private $_heure_convention_centre;
+		private $_heure_convention_entreprise;
+		private $_intitule;
+		private $_code_marche;
+		private $_donneur_ordre;
+		private $_date_debut_marche;
+		private $_date_fin_marche;
+	
+		public function __contruct(){
+			
+		}
+		
+		
+   }
+?>
