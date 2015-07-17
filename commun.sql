f49c2657a85404ca2d1afdf70469754bd46fb68b
Fort Michel <truc@machin.pouet>
2015-07-17 09:10:03 +0200

    modif fichier commun

--- commun.sql
+++ commun.sql
@@ -2,3 +2,17 @@
 	id SERIAL PRIMARY KEY,
 	intitule VARCHAR
 );
+
+ALTER TABLE formateur ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE formateur ADD COLUMN create_date DATE;
+ALTER TABLE formateur ADD COLUMN update_date DATE;
+ALTER TABLE formateur ADD COLUMN create_uid INT;
+ALTER TABLE formateur ADD COLUMN update_uid INT;
+ALTER TABLE formateur ADD COLUMN active BOOLEAN;
+
+ALTER TABLE feuille_presence ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE feuille_presence ADD COLUMN create_date DATE;
+ALTER TABLE feuille_presence ADD COLUMN update_date DATE;
+ALTER TABLE feuille_presence ADD COLUMN create_uid INT;
+ALTER TABLE feuille_presence ADD COLUMN update_uid INT;
+ALTER TABLE feuille_presence ADD COLUMN active BOOLEAN;
