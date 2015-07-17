fd784653a24ff376734ec3a319f5cb2583cf562d
Maxo <maxo@hotmail.cat>
2015-07-17 18:03:51 +0200

    creation class1

---
+++ model.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Model (Généralités) class mère
+ */
+class Model {
+	
+	function __construct() {
+		
+	}
+	
+	public function __set($key, $value){
+		
+		$this->{$key} = $value;
+		return $this;
+	
+	}
+	
+	public function __get($key)
+	{
+
+		return $this->{$key};
+
+	}
+	
+}
---
+++ traineecompany.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 
+ */
+class TraineeCompany extends Model {
+	
+	private $_attribute;
+	private $_evaluation;
+	private $_begin;
+	private $_end;
+	private $_job;
+	
+	function __construct() {
+		
+		
+	}
+}
