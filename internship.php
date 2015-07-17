8b3ac1f0943acbc014b21366a15612dcc75972dd
Maxo <maxo@hotmail.cat>
2015-07-17 16:31:08 +0200

    creation des class

---
+++ internship.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Classe Stage
+ */
+class Internship {
+	
+	public $id;
+	private $_internshipName;
+	private $_descriptif;
+	private $_internshipLevel;
+	private $_internshipTrainer;
+	private $_disponibility;
+	private $_validity;
+	private $_pay; //remuneration bool
+	private $_wage; //montant remuneration
+	
+	function __construct() {
+		
+	}
+}
