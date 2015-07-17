4da5266e69bdefee2752e26dba358374e10f64dc
Arthur <arthurbouchard76@gmail.com>
2015-07-17 14:05:43 +0200

    premiere version du fichier

---
+++ Controllers/Trainer.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('Person.php');
+
+class Trainer extends Person
+{
+	// Constructeur
+	public function __construct()
+	{
+		$this->_furtherInformations = 'Vide';
+	}
+	
+	//Attributs
+	private $_furtherInformations;
+	
+	// Getters & Setters
+	public function getFurtherInformations()
+	{
+		return $this->_furtherInformations;
+	}
+	
+	public function setFurtherInformations($furtherInformations)
+	{
+		$this->_furtherInformations = $furtherInformations;
+		return $this;
+	}
+}
+
+?>
