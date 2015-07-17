7dcf1de0b406cf81d5756ac8f5d71742ec1d180a
Thierry de Andrade <thierry.deandrade@gmail.com>
2015-07-17 12:19:25 +0200

    ajout de commentaire

--- Formation.php
+++ Formation.php
@@ -3,6 +3,14 @@
     //On ajoute abstract devant class si la classe est abstraite et qu'on ne veut pas l'appelé directement
     class Formation{
         
+        
+        //On peut définir des constantes comme ceci
+        CONST TYPE = "Student";
+        //-----------------------------------------
+        //On peut définir un attribut static, comme CONST mais en modifiable
+        static $test;
+        //On fera appel a lui via self::, récuperable de l'extérieur car sur la classe
+        //-----------------------------------------
         private $_id;
         private $_intitule;
         private $_effectif_moyen;
@@ -17,6 +25,7 @@
         public function __construct($heure){
             
             $this -> _heure_convention_centre = $heure;
+            //Si on veux crée une entité fille via l'construc de la mere
             //parent::__construct($heure)
             
         }
