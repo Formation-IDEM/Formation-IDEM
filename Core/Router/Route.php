e7dcbcd9e30a02bc88f50fd32b0a15ee37bbaedb
Alexandre Ribes <ribes.alexandre@gmail.com>
2015-07-19 19:11:54 +0200

    Meilleure méthode pour les controllers

--- Core/Router/Route.php
+++ Core/Router/Route.php
@@ -57,7 +57,7 @@ class Route
 	{
 		if( is_string($this->callable) )
 		{
-			$params = explode('#', $this->callable);
+			$params = explode('@', $this->callable);
 			$controller = "App\\Controllers\\" . $params[0] . "Controller";
 			$controller = new $controller();
 			return call_user_func_array([$controller, $params[1]], $this->matches);
