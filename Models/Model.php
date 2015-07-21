72c9445578994a2f74737de09a02081608070aba
Arthur <arthurbouchard76@gmail.com>
2015-07-21 08:36:24 +0200

    message cool !

---
+++ Models/Model.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Model
+{
+	function load() // charge un objet depuis la bdd
+	
+	function store() // associe un retour de form (post) sur un objet
+	
+	function save() // enregistre l'objet en bdd
+	
+	function delete() // supprime un objet en bdd
+	
+}
+
+?>
