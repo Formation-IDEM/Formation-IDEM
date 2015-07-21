d89048307600f085554638e352a46b160691943a
Arthur <arthurbouchard76@gmail.com>
2015-07-21 09:48:22 +0200

    fuck

--- SQL/commun_table-lancer-en-premier.sql
+++ SQL/commun_table-lancer-en-premier.sql
@@ -9,7 +9,7 @@ CREATE TABLE formation_sessions
 	id SERIAL PRIMARY KEY,
 	formations_id INT PRIMARY KEY,
 	begin_date DATE NOT NULL, -- date début d'une session de formation
-	ending_date DATE NOT NULL, -- date fin de sesion de formation
+	ending_date DATE NOT NULL -- date fin de sesion de formation
 );
 
 /* 2 tables, et c'est tout !, si vous avez un problème voyez ça avec Thierry ;) */
