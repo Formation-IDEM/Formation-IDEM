6ab167d53af4265f52f1b51cbd1c664e0847f0ec
Alexandre Ribes <ribes.alexandre@gmail.com>
2015-07-21 08:51:38 +0200

    Anglophonisation des tables

--- sql/entreprises.sql
+++ sql/entreprises.sql
@@ -1,47 +1,56 @@
-﻿CREATE TABLE entreprises (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(120) UNIQUE NOT NULL,
-	raison_sociale VARCHAR(120) NOT NULL,
-	secteur VARCHAR NOT NULL,
-	adresse VARCHAR(250) NOT NULL,
-	code_postal VARCHAR(6) DEFAULT NULL,
-	ville VARCHAR(60) NOT NULL,
-	pays VARCHAR(60) NOT NULL,
-	telephone VARCHAR(15) NOT NULL,
-	mobile VARCHAR(15) DEFAULT NULL,
-	fax VARCHAR(15) DEFAULT NULL,
-	dirigeant VARCHAR(60) DEFAULT NULL,
-	create_date TIMESTAMP DEFAULT(NOW()),
-	update_date TIMESTAMP CHECK(update_date >= create_date) DEFAULT(NOW()),
-	create_uid INT NOT NULL,
-	update_uid INT DEFAULT NULL,
-	date_visite TIMESTAMP DEFAULT NULL,
-	active BOOLEAN DEFAULT(true)
+﻿/**
+Table des entreprises
+ */
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(120) UNIQUE NOT NULL,        -- nom de l'entreprise
+  status VARCHAR(120) NOT NULL,             -- raison sociale
+  company_name VARCHAR(120) DEFAULT NULL,   -- nom commercial
+  address VARCHAR(250) NOT NULL,
+  postal_code VARCHAR(6) DEFAULT NULL,
+  city VARCHAR(60) NOT NULL,
+  country VARCHAR(60) NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  mobile VARCHAR(15) DEFAULT NULL,
+  fax VARCHAR(15) DEFAULT NULL,
+  manager_id VARCHAR(60) DEFAULT NULL,
+  create_date TIMESTAMP DEFAULT(NOW()),
+  update_date TIMESTAMP CHECK(update_date >= create_date) DEFAULT(NOW()),
+  create_uid INT NOT NULL,
+  update_uid INT DEFAULT NULL,
+  visit_date DATE DEFAULT NULL,
+  active BOOLEAN DEFAULT(true)
 );
 
-CREATE TABLE stages (
-	id SERIAL PRIMARY KEY,
-	titre VARCHAR(120) NOT NULL,
-	entreprise_id INT REFERENCES entreprises(id),
-	formation_id INT(4) REFERENCES session_formation(id),
-	descriptif TEXT DEFAULT NULL,
-	referent VARCHAR(60) DEFAULT NULL,
-	create_date TIMESTAMP DEFAULT(NOW()),
-	update_date TIMESTAMP CHECK(update_date >= create_date) DEFAULT(NOW()),
-	create_uid INT NOT NULL,
-	update_uid INT NOT NULL,
-	active BOOLEAN DEFAULT(true),
-	remuneration BOOLEAN DEFAULT(false),
-	indemnisation NUMERIC DEFAULT(0)
+/**
+Table des stages
+ */
+CREATE TABLE internships (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(120) NOT NULL,
+  company_id INT REFERENCES companies(id),
+  formation_id INT(4) REFERENCES formations(id),
+  explain TEXT DEFAULT NULL,
+  referent_id VARCHAR(60) REFERENCES trainers(id),
+  create_date TIMESTAMP DEFAULT(NOW()),
+  update_date TIMESTAMP CHECK(update_date >= create_date) DEFAULT(NOW()),
+  create_uid INT NOT NULL,
+  update_uid INT NOT NULL,
+  active SMALLINT DEFAULT(0),
+  pay BOOLEAN DEFAULT(false),
+  wage NUMERIC DEFAULT(0)
 );
 
-CREATE TABLE stagiaire_entreprise (
-	stagiaire_id INT REFERENCES stagiaire(id),
-	entreprise_id INT REFERENCES entreprises(id),
-	stage_id INT REFERENCES  stages(id),
-	en_cours BOOLEAN DEFAULT(false),
-	embauche BOOLEAN DEFAULT(false),
-	nombre_heures INT NOT NULL DEFAULT(0),
-	date_debut TIMESTAMP DEFAULT(NOW()),
-	date_fin TIMESTAMP CHECK(date_fin >= date_debut) DEFAULT(NOW())
+/**
+Table de liaison entre les entreprises, les stagiaires et les stages
+ */
+CREATE TABLE company_internship(
+  trainee_id INT REFERENCES trainees(id),
+  company_id INT REFERENCES companies(id),
+  internship_id INT REFERENCES internships(id),
+  active BOOLEAN DEFAULT(false),
+  hiring BOOLEAN DEFAULT(false),
+  total_hours INT NOT NULL DEFAULT(0),
+  date_begin TIMESTAMP DEFAULT(NOW()),
+  date_end TIMESTAMP CHECK(date_end >= date_begin) DEFAULT(NOW())
 );
