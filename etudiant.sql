622a0dfa36e2c06bb078da3a7c76fc9e6847563d
jiwibi <jybaudin@gmail.com>
2015-07-21 09:06:23 +0200

    Mise a jour anglais commande sql

--- etudiant.sql
+++ etudiant.sql
@@ -1,59 +1,59 @@
 ﻿-- création tables
 
-CREATE TABLE situation_famille (
+CREATE TABLE family_status (
 	id SERIAL PRIMARY KEY,
 	libelle VARCHAR
 );
 
-CREATE TABLE nationalite (
+CREATE TABLE nationality (
 	id SERIAL PRIMARY KEY,
 	libelle VARCHAR
 );
 
-CREATE TABLE type_remuneration (
+CREATE TABLE remuneration_type (
 	id SERIAL PRIMARY KEY,
 	libelle VARCHAR
 );
 
 
-CREATE TABLE stagiaire (
+CREATE TABLE trainee (
 	id SERIAL PRIMARY KEY,
-	nom VARCHAR NOT NULL,
-	prenom VARCHAR NOT NULL,
-	date_naissance DATE NOT NULL,
-	lieu_naissance VARCHAR NOT NULL,
-	sexe BOOLEAN NOT NULL,
-	adresse_off_rue VARCHAR NOT NULL,
-	adresse_off_complement VARCHAR NOT NULL,
-	adresse_off_codpost VARCHAR NOT NULL,
-	adresse_off_ville VARCHAR NOT NULL,
-	adresse_form_rue VARCHAR NOT NULL,
-	adresse_form_complement VARCHAR NOT NULL,
-	adresse_form_codpost VARCHAR NOT NULL,
-	adresse_form_ville VARCHAR NOT NULL,
-	telepĥone VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	mobile VARCHAR NOT NULL,
-	numero_securite_sociale INTEGER NOT NULL,
+	name VARCHAR NOT NULL,
+	firstname VARCHAR NOT NULL,
+	birthday DATE NOT NULL,
+	birthdayplace VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	adress_off_street VARCHAR NOT NULL,
+	adress_off_complement VARCHAR NOT NULL,
+	adress_off_codpost VARCHAR NOT NULL,
+	adress_off_city VARCHAR NOT NULL,
+	adress_form_street VARCHAR NOT NULL,
+	adress_form_complement VARCHAR NOT NULL,
+	adress_form_codpost VARCHAR NOT NULL,
+	adress_form_city VARCHAR NOT NULL,
+	phone VARCHAR NOT NULL,
+	mail VARCHAR NOT NULL,cours
+	cellphone VARCHAR NOT NULL,
+	social_security_number INTEGER NOT NULL,
 	photo VARCHAR NOT NULL,
-	id_type_remuneration INTEGER NOT NULL,
-	FOREIGN KEY (id_type_remuneration) REFERENCES type_remuneration(id),
-	id_statut_stagiaire INTEGER NOT NULL,
-	FOREIGN KEY (id_statut_stagiaire) REFERENCES statut_stagiaire(id),
+	id_remuneration_type INTEGER NOT NULL,
+	FOREIGN KEY (id_remuneration_type) REFERENCES remuneration_type(id),
+	id_status_stagiaire INTEGER NOT NULL,
+	FOREIGN KEY (id_trainee_status) REFERENCES trainee_status(id),
 	id_niveau_etude INTEGER NOT NULL,
-	FOREIGN KEY (id_niveau_etude) REFERENCES niveau_etude(id),
-	id_situation_famille INTEGER NOT NULL,
-	FOREIGN KEY (id_situation_famille) REFERENCES situation_famille(id),
-	id_nationalite INTEGER NOT NULL,
-	FOREIGN KEY (id_nationalite) REFERENCES nationalite(id)
+	FOREIGN KEY (id_study_level) REFERENCES study_level(id),
+	id_family_status INTEGER NOT NULL,
+	FOREIGN KEY (id_family_status) REFERENCES family_status(id),
+	id_nationality INTEGER NOT NULL,
+	FOREIGN KEY (id_nationality) REFERENCES nationality(id)
 ); 
 
-CREATE TABLE session_stagiaire (
+CREATE TABLE session_trainee (
 	id_session INTEGER NOT NULL,
-	id_stagiaire INTEGER NOT NULL,
-	PRIMARY KEY (id_session,id_stagiaire),
-	nb_heure_effectuee_cours INTEGER NOT NULL,
-	nb_heure_effectuee_stage INTEGER NOT NULL	
+	id_trainee INTEGER NOT NULL,
+	PRIMARY KEY (id_session,id_trainee),
+	nb_hour_present_school INTEGER NOT NULL,
+	nb_hour_present_trainer INTEGER NOT NULL	
 );
 
 
