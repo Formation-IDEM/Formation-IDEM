c0151b9704533147c4722f39631cffd3d90fd006
Julien Delacourt <jaydelacofficial@gmail.com>
2015-07-21 08:45:24 +0200

    ajout commentaires et traduction by Julien

--- formation.sql
+++ formation.sql
@@ -1,26 +1,26 @@
-﻿CREATE TABLE formation	(
+﻿CREATE TABLE formations	(
 	id SERIAL PRIMARY KEY, 
-	title VARCHAR,
-	average_effective INT,
-	convention_hour_center TIME,
-	convention_hour_compagny TIME,
-	deal_code VARCHAR,
-	order_giver VARCHAR,
-	deal_begin_date DATE,
-	deal_ending_date DATE
+	title VARCHAR NOT NULL,
+	average_effective INT, -- nombre d'élève par formation
+	convention_hour_center TIME NOT NULL, -- nombre d'heures conventionnées en centre
+	convention_hour_compagny TIME NOT NULL, --nombre d'heures conventionnées en entreprise 
+	deal_code VARCHAR NOT NULL, -- code de marché 
+	order_giver VARCHAR, -- donneur d'ordre
+	deal_begin_date DATE NOT NULL, -- date début du marché
+	deal_ending_date DATE NOT NULL -- date fin de marché
 );
 
-CREATE TABLE formation_session 	(
-	formation_id INT PRIMARY KEY,
-	begin_date DATE,
-	ending_date DATE,
-	FOREIGN KEY (formation_id) REFERENCES formation(id)
+CREATE TABLE formation_sessions 	(
+	formations_id INT PRIMARY KEY,
+	begin_date DATE NOT NULL, -- date début d'une session de formation
+	ending_date DATE NOT NULL, -- date fin de sesion de formation
+	FOREIGN KEY (formations_id) REFERENCES formations(id)
 );
 
 CREATE TABLE ref_pedago(
-	formation_id INT,
-	matter_id INT,
-	PRIMARY KEY (formation_id, matiere_id),
-	FOREIGN KEY (formation_id) REFERENCES formation(id),
-	FOREIGN KEY (matiere_id) REFERENCES matiere(id)
+	formations_id INT,
+	matters_id INT,  -- code matiere
+	PRIMARY KEY (formations_id, matters_id),
+	FOREIGN KEY (formations_id) REFERENCES formations(id),
+	FOREIGN KEY (matters_id) REFERENCES matters(id)
 );
