a0a4f208ae8d9c483f40478296d03e2c786915f8
jiwibi <jybaudin@gmail.com>
2015-07-21 09:30:43 +0200

    requete normalisation

--- etudiant.sql
+++ etudiant.sql
@@ -1,22 +1,4 @@
-﻿-- création tables
-
-CREATE TABLE family_status (
-	id SERIAL PRIMARY KEY,
-	libelle VARCHAR
-);
-
-CREATE TABLE nationality (
-	id SERIAL PRIMARY KEY,
-	libelle VARCHAR
-);
-
-CREATE TABLE remuneration_type (
-	id SERIAL PRIMARY KEY,
-	libelle VARCHAR
-);
-
-
-CREATE TABLE trainee (
+CREATE TABLE trainees (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR NOT NULL,
 	firstname VARCHAR NOT NULL,
@@ -36,22 +18,24 @@ CREATE TABLE trainee (
 	cellphone VARCHAR NOT NULL,
 	social_security_number INTEGER NOT NULL,
 	photo VARCHAR NOT NULL,
-	id_remuneration_type INTEGER NOT NULL,
-	FOREIGN KEY (id_remuneration_type) REFERENCES remuneration_type(id),
-	id_status_stagiaire INTEGER NOT NULL,
-	FOREIGN KEY (id_trainee_status) REFERENCES trainee_status(id),
-	id_niveau_etude INTEGER NOT NULL,
-	FOREIGN KEY (id_study_level) REFERENCES study_level(id),
-	id_family_status INTEGER NOT NULL,
-	FOREIGN KEY (id_family_status) REFERENCES family_status(id),
-	id_nationality INTEGER NOT NULL,
-	FOREIGN KEY (id_nationality) REFERENCES nationality(id)
+	remuneration_type_id INTEGER NOT NULL,
+	FOREIGN KEY (remuneration_type_id) REFERENCES remuneration_types(id),
+	status_trainee_id INTEGER NOT NULL,
+	FOREIGN KEY (status_trainee_id) REFERENCES trainee_status(id),
+	study_levels_id INTEGER NOT NULL,
+	FOREIGN KEY (study_level_id) REFERENCES study_levels(id),
+	family_status_id INTEGER NOT NULL,
+	FOREIGN KEY (family_status_id) REFERENCES family_status(id),
+	nationality_id INTEGER NOT NULL,
+	FOREIGN KEY (nationality_id) REFERENCES nationalities(id)
 ); 
 
-CREATE TABLE session_trainee (
-	id_session INTEGER NOT NULL,
-	id_trainee INTEGER NOT NULL,
-	PRIMARY KEY (id_session,id_trainee),
+CREATE TABLE sessions_trainee (
+	id SERIAL PRIMARY KEY,
+	session_id INTEGER NOT NULL,
+	FOREIGN KEY (session_id) REFERENCES session(id),
+	trainee_id INTEGER NOT NULL,
+	FOREIGN KEY (trainee_id) REFERENCES trainees(id),
 	nb_hour_present_school INTEGER NOT NULL,
 	nb_hour_present_trainer INTEGER NOT NULL	
 );
