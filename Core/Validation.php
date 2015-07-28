d36c75158c7079dffa71a2a331dbc9b03eb04de0
Alexandre Ribes <ribes.alexandre@gmail.com>
2015-07-28 16:59:41 +0200

    Ajout de Validation.php

---
+++ Core/Validation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Validation.php
+ * ------------
+ * @author :  RIBES Alexandre
+ * @contact : ribes.alexandre@gmail.com
+ * @website : http://www.alexandre-ribes.fr
+ */
+
+namespace Core;
+
+
+class Validation
+{
+
+}
