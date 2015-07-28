fdabc3507ee64da70c899169c448d24beee99bc0
Arthur <arthurbouchard76@gmail.com>
2015-07-28 11:03:07 +0200

    modif index.php

--- Controllers/FrontController.php
+++ Controllers/FrontController.php
@@ -1,26 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-
-class FrontController {
-
-
-	/*
-	 * 
-	 */
-	function indexAction() {
-		// indexAction - FrontController
-		echo "indexAction - FrontController";
-		
-		$fs = App::getModel('FamilyStatus');
-		
-		$fs->store(array( 'title' => 'celibataire'));
-		
-		var_dump( $fs->save() );
-	}
-
-
-=======
 class FrontController
 {
 	function indexAction()
@@ -42,7 +21,6 @@ class FrontController
 		//$trainer->load(4)->delete();
 		//var_dump($trainer);
 	}
->>>>>>> zearg
 }
 
 ?>
