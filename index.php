c2abc0956172ace3811ac815a8455fc65af269cc
Thierry de Andrade <thierry.deandrade@gmail.com>
2015-09-04 09:25:20 +0200

    ajout du Header et footer via render

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<head>
-    
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-
-</head>
 <?php
 	
 	include_once('App.php');
