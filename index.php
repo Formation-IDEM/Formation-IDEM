168c1b99ef4d57e22804f4ec330b692223c980d9
Cracknet <ariandor.lancedragon@gmail.com>
2015-09-08 08:50:18 +0200

    reset

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-   include_once('Formation.php');
-   include_once('SessionFormation.php');
-   
-   $cap_metier = new sessionformation();
-   
-   echo $cap_metier -> getDateDebut();
-   
-?>
